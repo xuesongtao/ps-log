70bc6602137ca8a8b641f2a56f85db7fe664e4e8
xuesongtao <xuesongtao512@qq.com>
2023-01-06 13:33:40 +0800

    update

--- match.go
+++ match.go
@@ -8,18 +8,17 @@ type node struct {
 	IsRoot      bool
 	IsEnd       bool
 	Data        byte
-	ChildrenMap map[byte]*node
+	ChildrenMap [128]*node
 }
 
-func NewNode(b byte, root ...bool) *node {
+func newNode(b byte, root ...bool) *node {
 	isRoot := false
 	if len(root) > 0 && root[0] {
 		isRoot = root[0]
 	}
 	return &node{
-		IsRoot:      isRoot,
-		Data:        b,
-		ChildrenMap: make(map[byte]*node),
+		IsRoot: isRoot,
+		Data:   b,
 	}
 }
 
@@ -34,7 +33,7 @@ type tire struct {
 
 func newTire() *tire {
 	// 根节点设置为 '/'
-	return &tire{root: NewNode('/', true)}
+	return &tire{root: newNode('/', true)}
 }
 
 // insert 新增模式串
@@ -43,8 +42,8 @@ func (t *tire) insert(bytes []byte) {
 	curNode := t.root
 	for i := 0; i < dataLen; i++ {
 		b := bytes[i]
-		if _, ok := curNode.ChildrenMap[b]; !ok {
-			curNode.ChildrenMap[b] = NewNode(b)
+		if node := curNode.ChildrenMap[b]; node == nil {
+			curNode.ChildrenMap[b] = newNode(b)
 		}
 		curNode = curNode.ChildrenMap[b]
 	}
@@ -57,7 +56,7 @@ func (t *tire) search(target []byte) bool {
 	curNode := t.root
 	for i := 0; i < dataLen; i++ {
 		b := target[i]
-		if _, ok := curNode.ChildrenMap[b]; !ok {
+		if node := curNode.ChildrenMap[b]; node == nil {
 			if curNode.IsEnd { // 匹配
 				break
 			}
